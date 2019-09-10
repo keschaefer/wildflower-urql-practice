f565a0dcd43a0a2cb507035d42719d935d083a58
Kate Schaefer <kate.schaefer@formidable.com>
2019-09-10 14:44:41 -0600

    added urql, material-ui, additional directories

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql'
+import { cacheExchange } from '@urql/exchange-graphcache'
+import { suspenseExchange } from '@urql/exchange-suspense'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = cacheExchange({})
+const client = new Client({
+  url: 'http://localhost:4000',
+  exchanges: [dedupExchange, suspenseExchange, cache, fetchExchange],
+  suspense: true
+})
+
+ReactDOM.render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
