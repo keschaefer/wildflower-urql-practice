03de2cdf5ad666e430cd2805543a322740aa7f1f
Kate Schaefer <kate.schaefer@formidable.com>
2019-09-11 11:16:23 -0600

    add Header, add server repo

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import Main from './Main'
+import { Typography, Button, Toolbar, AppBar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+})
 
 function App() {
+  const classes = useStyles()
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6">Rocky Mountain Wildflowers</Typography>
+          <div>
+            <Button color="inherit">Sign In</Button>
+            <Button color="inherit">Sign Up</Button>
+          </div>
+        </Toolbar>
+      </AppBar>
       <Main />
     </div>
   )
